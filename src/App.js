e6c5e200e82dcc466966eb979f0ea1e24d75b454
Tait Hoyem <tait@tait.tech>
2020-05-22 14:29:02 +0000

    Add grid

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ function App() {
   return (
     <div className="App">
      <h1> this is a heroku test - DEPLOYMENT succeeded</h1>
-      <div>hello vaib</div>
-      <div>you need sleep</div>
+      
+      
+      <div id = "dtable">
+        <div>output 1</div>
+        <div>output 2</div>
+        <div>output 3</div>
+        <div>output 4</div>
+        <div>spare</div>
+      </div>
+
+
+
     </div>
   );
 }
