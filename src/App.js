ac82d613f4ea96df7c8d04e454ae406719241408
Tait Hoyem <tait@tait.tech>
2020-05-22 14:38:47 +0000

    Revert "Add grid"

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
      <h1> this is a heroku test - DEPLOYMENT succeeded</h1>
-      
-      
-      <div id = "dtable">
-        <div>output 1</div>
-        <div>output 2</div>
-        <div>output 3</div>
-        <div>output 4</div>
-        <div>spare</div>
-      </div>
-
-
-
+      <div>hello vaib</div>
+      <div>you need sleep</div>
     </div>
   );
 }
